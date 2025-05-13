72533b526dbab9b4a06f437f6895efa6fda55618
Mahiru Shiina <halovemusics@gmail.com>
2025-05-13 11:52:20 +0700

    Sửa layout

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
-require("dotenv").config();  
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
-
 const route = require("./routes/client/index.route");
 
-
-
 app.set("views", "./views");
-app.set("view engine", "pug")
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
 
 // Routes
 route(app);
