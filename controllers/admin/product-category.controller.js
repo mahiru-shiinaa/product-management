686e2f28f1506d4b11ec67ed617750f96e8eb98e
Mahiru Shiina <halovemusics@gmail.com>
2025-05-26 12:10:59 +0700

    Phân cấp category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,34 +1,57 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system");
+const createTree = require("../../helpers/createTree");
+
+
 //[GET] /admin/products-category
 module.exports.index = async (req, res) => {
-    let find = {
+  let find = {
     deleted: false,
   };
 
-  // limit để set hiển thị bao nhiêu sản phẩm, skip để bỏ qua bao nhiêu sản phẩm rồi mới hiển thị
-    const records = await ProductCategory.find(find);
+  
 
-    res.render(`admin/pages/products-category/index`, { pageTitle: "Danh sách danh mục", records: records });
-}
+
+  
+  const records = await ProductCategory.find(find);
+
+  const newRecords = createTree.tree(records);
+
+  res.render(`admin/pages/products-category/index`, {
+    pageTitle: "Danh sách danh mục",
+    records: newRecords,
+  });
+};
 
 //[GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
-    res.render("admin/pages/products-category/create", { pageTitle: "Tạo danh mục" });
-}
+  let find = {
+    deleted: false,
+  };
+ 
+
+  const records = await ProductCategory.find(find);
+
+  const newRecords = createTree.tree(records);
+  // console.log('newRecords', newRecords);
+
+  res.render("admin/pages/products-category/create", {
+    pageTitle: "Tạo danh mục",
+    records: newRecords,
+  });
+};
 
 //[POST] /admin/products-category/create-post
 module.exports.createPost = async (req, res) => {
-      // Kiểm tra thử có nhập position không
-  if(req.body.position === "")  {
+  // Kiểm tra thử có nhập position không
+  if (req.body.position === "") {
     const countProduct = await ProductCategory.countDocuments({});
     req.body.position = countProduct + 1;
   } else {
     req.body.position = parseInt(req.body.position);
   }
-    const record = new ProductCategory(req.body);
-  // // Lưu vào database 
-   await record.save();
-    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
-}
-
+  const record = new ProductCategory(req.body);
+  // // Lưu vào database
+  await record.save();
+  res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+};
