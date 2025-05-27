aea1846c6853e113caa9b6e21b6c29098fb89a6a
Mahiru Shiina <halovemusics@gmail.com>
2025-05-27 12:09:02 +0700

    Tạo model phân quyền

---
+++ models/role.model.js
@@ -0,0 +1,26 @@
+
+const mongoose = require("mongoose");
+
+
+// Tạo khung dữ liệu
+const roleSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    permissions: {
+        type: Array,
+        default: []
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date
+}, {
+    // Dùng để thêm thời gian tạo và cập nhật sản phẩm tự động
+    timestamps: true
+});
+
+// Tạo model                  Tên model   Tên khung dữ liệu   Tên collection
+const Role = mongoose.model("Role", roleSchema, "roles");
+
+module.exports = Product;
