cfe6191cbf3fa1bf5808f36160419d49728e0d1c
Mahiru Shiina <halovemusics@gmail.com>
2025-06-08 12:17:45 +0700

    Check phân quyền hiển thị nội dung

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -1,16 +1,21 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 module.exports.requireAuth = async (req, res, next) => {
-  console.log(req.cookies.token);
   if (!req.cookies.token) {
     return res.redirect("/admin/auth/login");
   } else {
-    const account = await Account.findOne({ token: req.cookies.token });
+    // .select("-password"); không trả về password
+    const account = await Account.findOne({ token: req.cookies.token }).select("-password");
 
     if (!account) {
       return res.redirect("/admin/auth/login");
     } else {
+        const role = await Role.findOne({ _id: account.role_id, deleted: false }).select("title permissions");
+        // Tạo account thành biến toàn cục
+      res.locals.account = account;  
+      res.locals.role = role;
       next();
     }
   }
