69f5f385f6c6f68c22ec5d3588d9860caaec11e4
Mahiru Shiina <halovemusics@gmail.com>
2025-06-10 11:54:45 +0700

    Lọc product theo category

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,7 @@
 // Khai báo model lấy dữ liệu
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
+const productCategoryHelper = require("../../helpers/products-category");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -34,6 +36,42 @@ module.exports.detail = async (req, res) => {
     res.render("client/pages/products/detail", {
       pageTitle: product.title,
       product: product,
+    });
+  } catch (error) {
+    res.redirect("/products");
+  }
+};
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  try {
+    const slug = req.params.slugCategory;
+    const category = await ProductCategory.findOne({
+      slug: slug,
+      status: "active",
+      deleted: false,
+    });
+    
+
+   const listSubCategory = await productCategoryHelper.getSubCategory(category.id);
+   const listSubCategoryIds = listSubCategory.map((item) => item.id);
+    const products = await Product.find({
+      product_category_id: {
+        $in: [category._id, ...listSubCategoryIds]
+      },
+      status: "active",
+      deleted: false,
+    }).sort({ position: "desc" });
+    const newProducts = products.map((item) => {
+      item.priceNew = (
+        (item.price * (100 - item.discountPercentage)) /
+        100
+      ).toFixed(0);
+      return item;
+    });
+    res.render("client/pages/products/index", {
+      pageTitle: category.title,
+      products: newProducts,
     });
   } catch (error) {
     res.redirect("/products");
